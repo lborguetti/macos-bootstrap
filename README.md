ff64932c23cdfa91a384f9fd7ed8c3f3c9d93e10
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2022-11-27 14:47:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# macos-workstation-bootstrap
+
+Keeping in mind the KISS principle this few lines of code can be used
+to create/setup an entire workspace without complexity. 
+
+This is a idempotent script used to provision my entire developer environment.
+
+Idempotent scripts can be called multiple times and each time it’s called, it will
+have the same effects on the system.
+
+To understand how to use this script I recommend you to read the code, after all, 
+it's not good practice to run a script that you don't know.
+
+Currently tested only in macOS Ventura 13+.
+
+Note/Tip: before to start use this script you need to install [Homebrew](https://docs.brew.sh/Installation).
---
+++ bootstrap.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+#
+# Author: Luciano Antonio Borguetti Faustino
+# Release Date: 29/10/2022
+#
+# This is a idempotent script used to provision my entire developer environment.
+#
+# Idempotent scripts can be called multiple times and each time it’s called, it will
+# have the same effects on the system.
+#
+# Currently tested only in macOS Ventura 13+ (Macbook Air M2).
+
+set -e errexit
+set -u nounset
+
+function file_changes(){
+
+    diff "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
+
+}
+
+function file_exists(){
+
+    stat "${HOME}/${*}" > /dev/null 2>&1 || false
+
+}
+
+function do_dotfile_install(){
+
+    _dirname=""
+    update_neovim=""
+
+    if echo "${*}" | grep ".vim$" > /dev/null ; then
+        update_neovim="true"
+    fi
+
+    _dirname=$(dirname "${*}") || false
+    if ! file_exists "${_dirname}"; then
+        mkdir -p "${HOME}/${_dirname}" > /dev/null || false
+    fi
+    cp -f "dotfiles/${*}" "${HOME}/${*}" > /dev/null 2>&1 || false
+
+}
+
+function do_dotfiles(){
+
+dotfile=""
+
+while IFS="" read -r dotfile; do
+    if ! file_exists "${dotfile}"; then
+        echo -n "Ensuring the copy of the ${dotfile} dotfile: "
+        if ! do_dotfile_install "${dotfile}"; then
+            echo "fail"
+        else
+            echo "ok"
+        fi
+    else
+        if ! file_changes "${dotfile}"; then
+            echo -n "The file was changed, ensuring the copy of the ${dotfile} dotfile: "
+            if ! do_dotfile_install "${dotfile}"; then
+                echo "fail"
+                exit 1
+            else
+                echo "ok"
+            fi
+        fi
+    fi
+done <<< "$(cd dotfiles && find . -type f)"
+
+}
+
+function package_exists(){
+
+    brew list "${@}" > /dev/null 2>&1 || false
+
+}
+
+function do_package_install(){
+
+    brew install --quiet "${@}" > /dev/null || false
+
+}
+
+function do_packages(){
+
+package=""
+
+while IFS="" read -r package; do
+    if ! package_exists "${package}"; then
+        echo -n "Ensuring the installation of the ${package} package: "
+        if ! do_package_install "${package}"; then
+            echo "fail"
+            exit 1
+        else
+            echo "ok"
+        fi
+    fi
+done < ./packages.txt
+
+}
+
+function do_neovim(){
+
+    nvim +PlugUpdate  +qall > /dev/null 2>&1 || false
+    nvim +PlugUpgrade +qall > /dev/null 2>&1 || false
+
+}
+
+do_dotfiles
+#do_packages
+if [ "${update_neovim:-}" == "true" ]; then
+    do_neovim
+fi
